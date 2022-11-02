46a8b506d81f12e5a3cc71307e2e96102d3e97a1
Midland707 <rus14job@ukr.net>
2022-11-02 23:13:32 +0200

    add autotest1

--- mod3-auto1.js
+++ mod3-auto1.js
@@ -1,3 +1,11 @@
 //MODUL3 AUTOTEST1
 
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+};
 
+console.log(apartment);
