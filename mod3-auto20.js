012f476e4d152fa023bd80ac5192259aeed9a5c6
Midland707 <rus14job@ukr.net>
2022-11-05 23:34:37 +0200

    add autotest 20-26

--- mod3-auto20.js
+++ mod3-auto20.js
@@ -1,1 +1,27 @@
 //MODUL3 AUTOTEST20
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  // Change code below this line
+  let totalPrice = 0;
+  for (const product of products) {
+    if (product.name == productName) {
+      totalPrice = product.price * product.quantity;
+    }
+  }
+  console.log(totalPrice);
+  return totalPrice;
+  // Change code above this line
+}
+
+calculateTotalPrice("Blaster"); //повертає 0
+calculateTotalPrice("Radar"); //повертає 5200
+calculateTotalPrice("Droid"); //повертає 2800
+calculateTotalPrice("Grip"); //повертає 10800
+calculateTotalPrice("Scanner"); //повертає 8100
--- mod3-auto23.js
+++ mod3-auto23.js
@@ -1,1 +1,17 @@
 //MODUL3 AUTOTEST23
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Change code below this line
+const {
+  yesterday: highYesterday,
+  today: highToday,
+  tomorrow: highTomorrow,
+  icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+} = highTemperatures;
+// Change code above this line
+const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+console.log(meanTemperature);
